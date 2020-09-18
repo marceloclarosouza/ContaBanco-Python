a5d3752e8f7c03b0dc4d23dc6b0209b6b6753e13
MarceloClaroSouza <marcelo.claro.souza@gmail.com>
2020-09-17 23:03:09 -0300

    metodos funcionando

--- conta_banco.py
+++ conta_banco.py
@@ -13,7 +13,38 @@ class Conta:
         self.__conta = Conta.contador
         Conta.contador += 1
         
+    def titular(self):
+        return f'Cliente: {self.__nome} {self.__sobrenome}, Conta: {self.__conta}'
+    
+    def saldo_c(self):
+        return f'O Saldo do Sr(a) {self.__nome} {self.__sobrenome} é de R$ {self.__saldo}'
+    
+    def limite_c(self):
+        return f'O limite do Sr(a) {self.__nome} {self.__sobrenome} é de R$ {self.__limite}'
+    
+    def sacar(self, valor):
+        self.__saldo -= valor
+        return f'Valor sacado: {valor}\tSeu saldo atual é de {self.__saldo}'
+    
+    def depositar(self, valor):
+        self.__saldo += valor
+        return f'Valor depositado: {valor}\tSeu saldo atual é de {self.__saldo}'
+    
+    def transferir(self, valor, destino):
+        """transfere um valor da conta A para a conta B"""
+        self.__saldo -= valor
+        destino.__saldo += valor##adiciona o valor a conta de destino
+        
     
     
-    
+ 
 cliente1 = Conta("marcelo", "souza", 1000, 3000)
+cliente2 = Conta("Michael", "Kiske", 9999, 9999)
+print(cliente1.limite_c())
+print(cliente1.saldo_c())
+print(cliente1.titular())
+print(cliente1.sacar(200))
+print(cliente1.depositar(500))
+
+cliente1.transferir(100, cliente2)
+print(cliente2.saldo_c())
